5c11d3c113ef1110f04611783e2611d42b75882e
masked (set in repo/.git/config) <masked (set in repo/.git/config)>
2023-02-18 21:27:37 +0100

    make the array count properly (pixels are multiple numbers)

--- image_to_uint16_array_4bpc_ARGB.py
+++ image_to_uint16_array_4bpc_ARGB.py
@@ -4,17 +4,18 @@ from PIL import Image
 from numpy import asarray, uint16, array
 
 img = Image.open(sys.argv[1])
-numpydata = asarray(img, 'uint16')
+numpydata = asarray(img.convert('RGB'), 'uint16')
+length = len([item for sub_list in numpydata for item in sub_list])
 
 def colorArrToARGB(pixel):
-    return hex((0xf<<12) + (pixel[0]<<8) + (pixel[1]<<4) + (pixel[2]))
+    return format((0x0<<12) + (pixel[0]<<8) + (pixel[1]<<4) + (pixel[2]), '#06x')
 
 def printRow(row):
     print('\t', end='')
     [print(f"{colorArrToARGB(x)},",end='') for x in row]
     print()
 
-print(f"const color_t spritesheet[{numpydata.size}] = \u007b")
+print(f"const color_t spritesheet[{length}] = \u007b")
 [printRow(x) for x in numpydata]
 print('};')
 
